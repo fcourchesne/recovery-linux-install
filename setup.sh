3fb227b58ddbfddeffdb779c7b8279df9cb9c550
Francis Courchesne <fcourchesne@gmail.com>
2021-03-28 00:11:26 -0400

    sync

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+#sudo pacman -S git ncdu tree tmux htop
+#sudo pacman -S neovim
+#sudo pacman -S xclip firefox unzip unrar ranger
+#sudo pacman -S zsh
+#git clone http://github.com/keenerd/packer.git packer.git
+#chmod +x packer.git/packer
+#sudo mv packer.git/packer /usr/bin/
+#rm -rf packer.git
+#sudo pacman -S jshon
+#packer -Syyu
+#packer -S plank i3 i3blocks blender gimp firefox base-devel avrdude gcc gdb llvm filezilla mpv
+#vlc &
+#plank &
+#packer -S hexchat irssi baobab
+#packer -S mpd ncmpcpp mpc
+#packer -S libreoffice-fresh
+#packer -S libreoffice-fresh-{fr,ru}
+#packer -S libreoffice-fresh-zh-cn
+#packer -S krita
+#packer -S freecad kicad kicad-library 
+#packer -S wine winetricks
+#winetricks corefonts gecko
+#packer -S ltspice
+packer -S python-pandas python-numpy python-matplotlib python-ipdb 
+
+
+#python-plotly
+#python-request
+#packer -S typescript
+#packer -S calibre
+#python postgres
+
